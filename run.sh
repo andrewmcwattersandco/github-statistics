44faadfdc41f9c13a1c925e76e7d9f654dd7f762
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Pro.local>
2022-04-28 11:26:17 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+ls -la
+remaining=1
+while [ $remaining -gt 0 ]
+do
+  since=$(sqlite3 github-statistics.db 'SELECT id FROM users ORDER BY id DESC LIMIT 1')
+  if curl \
+    -D store.txt \
+    --fail-with-body \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H 'Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' \
+    -S \
+    -s \
+    "https://api.github.com/users?per_page=100&since=${since}" > users.json
+  then
+    sqlite-utils insert github-statistics.db users - --pk=id < users.json
+  else
+    reset=$(grep 'x-ratelimit-reset' store.txt)
+    echo "$reset"
+    reset=$(echo "$reset" | tr -cd '[:digit:]')
+    grep 'message' store.txt
+  fi
+
+  remaining=$(grep 'x-ratelimit-remaining' store.txt)
+  echo "$remaining"
+  remaining=$(echo "$remaining" | tr -cd '[:digit:]')
+done
