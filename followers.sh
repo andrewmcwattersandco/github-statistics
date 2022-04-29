52cd19f442f74b432b60f9c2dbe6c6a959056055
Andrew McWatters <andrewmcwatters@Andrews-MacBook-Pro.local>
2022-04-29 13:25:10 -0700

    Create followers.sh

---
+++ followers.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+id=0
+remaining=1
+while [ -n "$remaining" ] && [ $remaining -gt 0 ]
+do
+  # Get user
+  query="\
+    SELECT id, followers_url \
+    FROM users \
+    WHERE id > ${id} \
+    ORDER BY id \
+    LIMIT 1"
+  result=$(sqlite3 github-users.db "$query")
+
+  # id|followers_url
+  id=$(echo "$result" | cut -f 1 -d '|')
+  followers_url=$(echo "$result" | cut -f 2 -d '|')
+
+  # 0 rows
+  if [ -z "$followers_url" ]
+  then
+    exit
+  fi
+
+  # Get followers
+  if
+    curl \
+      -D store.txt \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: Bearer $GITHUB_TOKEN" \
+      -S \
+      -s \
+      "$followers_url?per_page=100" \
+      > followers.json
+    grep -q 'HTTP/2 200' store.txt
+  then
+    # Insert followers
+    # sqlite-utils insert github-users.db users-followers - --pk=id \
+    # < followers.json
+
+    # TODO: Get follower count
+    grep -Eo 'link: <.+>; rel="next", <.+>; rel="last"' < store.txt | \
+    cut -f 2 -d ','
+
+    # TODO: Get `page` parameter
+    page=0
+    pages=$((page - 1))
+    count=$((100 * pages))
+
+    # TODO: Get number of followers on the last page
+    followers=0
+    count=$((count + followers))
+  else
+    # The time at which the current rate limit window resets in UTC epoch
+    # seconds.
+    reset=$(grep 'x-ratelimit-reset' store.txt)
+    echo "$reset"
+    reset=$(echo "$reset" | tr -cd '[:digit:]')
+
+    # `x-ratelimit-reset` - current time
+    remaining=$((reset - $(date +%s)))
+    if [ $remaining -gt 0 ]
+    then
+        echo "$remaining seconds until rate limit reset"
+    fi
+
+    # { "message": ".*" } -> message: .*
+    grep -Eo '("message":)\s*(".*")' users.json | tr -d '"'
+    exit
+  fi
+
+  # The number of requests remaining in the current rate limit window.
+  remaining=$(grep 'x-ratelimit-remaining' store.txt)
+  echo "$remaining"
+  remaining=$(echo "$remaining" | tr -cd '[:digit:]')
+done
